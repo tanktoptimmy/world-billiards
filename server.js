e367ebae3bc636590fc2eea5d88097c8cb1b1df4
Timothy Parry <tim.parry@skybettingandgaming.com>
2019-01-27 17:04:22 +0000

    uppercasing PORT

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const app = express();
 const dev = app.get("env") !== "production";
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 if (!dev) {
     app.disable("x-powered-by");
