cbff2c1255d565baf33dcd05a79f67ce8d78e8f7
Timothy Parry <tim.parry@skybettingandgaming.com>
2019-01-27 17:21:56 +0000

    adding socket.io

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { createServer } = require("http");
+const { createServer, Server } = require("http");
 const express = require("express");
 const compression = require("compression");
 const morgan = require("morgan");
@@ -9,6 +9,20 @@ const dev = app.get("env") !== "production";
 
 const normalisePort = port => parseInt(port, 10);
 const PORT = normalisePort(process.env.PORT || 5000);
+
+
+// Socket.io
+const http = Server(app);
+const io = require('socket.io')(http);
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('User Disconnected');
+  });
+  socket.on('send_scoreboard_update', function(update){
+    socket.broadcast.emit('scoreboard_update', update);
+  });
+});
 
 if (!dev) {
     app.disable("x-powered-by");
